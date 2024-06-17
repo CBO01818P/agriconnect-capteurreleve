e0d5d7f14ad7b4615654c26a03f99b12ec04ada2
Martin Billard <martin.billard@toulouse.miage.fr>
2024-06-17 22:16:00 +0200

    Refactor routes des FeignClient pour correspondre à la norme

--- src/main/java/ag/capteurreleve/entities/ClientCapteur.java
+++ src/main/java/ag/capteurreleve/entities/ClientCapteur.java
@@ -4,18 +4,21 @@ import ag.capteurreleve.transientObj.Capteur;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @FeignClient(name = "agriconnect-capteur")
 public interface ClientCapteur {
-    @GetMapping("/api/capteurs")
+
+    @RequestMapping(value = "/api/capteurs", method = RequestMethod.GET)
     List<ClientCapteur> getAllCapteurs();
 
-    @GetMapping("/api/capteurs/{id}")
+    @RequestMapping(value = "/api/capteurs/{id}", method = RequestMethod.GET)
     Capteur findById(@PathVariable("id") Long id);
 
-    @GetMapping("/api/capteurs/mesure/{id}")
+    @RequestMapping(value = "/api/capteurs/mesure/{id}", method = RequestMethod.GET)
     ClientCapteur getMesure(@PathVariable("id") Long id);
 
 }
